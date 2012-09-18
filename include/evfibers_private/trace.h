b4e17272eeffa27257da78bfbf96507746c76ff0
Konstantin Olkhovskiy <lupus@oxnull.net>
2012-09-19 00:21:50 +0400

    Fixed copyright boilerplates

--- include/evfibers_private/trace.h
+++ include/evfibers_private/trace.h
@@ -2,20 +2,21 @@
 
   Copyright 2012 Konstantin Olkhovskiy <lupus@oxnull.net>
 
-  This file is part of Mersenne.
+  This file is part of libevfibers.
 
-  Mersenne is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation, either version 3 of the License, or
-  any later version.
+  libevfibers is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as
+  published by the Free Software Foundation, either version 3 of the
+  License, or any later version.
 
-  Mersenne is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  libevfibers is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with Mersenne.  If not, see <http://www.gnu.org/licenses/>.
+  You should have received a copy of the GNU Lesser General
+  Public License along with libevfibers.  If not, see
+  <http://www.gnu.org/licenses/>.
 
  ********************************************************************/
 
