ed8aab53aeea5e7e30c90d2fd739e04b2a6795b3
Konstantin Olkhovskiy <lupus@oxnull.net>
2012-10-13 17:01:05 +0400

    Removed extra files from lcov report

--- test_coverage.sh
+++ test_coverage.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+function remove_extra {
+	lcov -q --remove $1 "coro/*" -o $1
+	lcov -q --remove $1 "coverage_build/CMakeFiles/*" -o $1
+	lcov -q --remove $1 "/usr/include/*" -o $1
+}
+
 if [ -d coverage_build ] ; then
 	rm -rf coverage_build
 fi
@@ -10,8 +16,10 @@ pushd coverage_build
 CC=/usr/bin/gcc CFLAGS="-fprofile-arcs -ftest-coverage" cmake -DCMAKE_BUILD_TYPE=Debug ..
 make
 lcov -c -i -d . -o evfibers_base.info
-./test/evfibers_test
+remove_extra evfibers_base.info
+make test
 lcov -c -d . -o evfibers_test.info
+remove_extra evfibers_test.info
 lcov -a evfibers_base.info -a evfibers_test.info -o evfibers_total.info
 mkdir html
 genhtml -o html -b evfibers_base.info -t "libevfibers" --highlight evfibers_total.info
