7a7849c15c6a59744dbf1d73b2651910e7cfc92a
Konstantin Olkhovskiy <kolkhovskiy@nvidia.com>
2015-12-27 00:38:50 +0300

    Add support for detecting libeio as ev-eio

--- cmake/FindLibEio.cmake
+++ cmake/FindLibEio.cmake
@@ -4,11 +4,15 @@ find_path(LIBEIO_INCLUDE_DIR eio.h
 	PATHS /usr/local /usr
 )
 find_library(LIBEIO_LIBRARY
-  NAMES eio
+  NAMES ev-eio eio
   HINTS $ENV{LIBEIO_DIR}
   PATH_SUFFIXES lib
   PATHS /usr/local /usr
 )
+check_library_exists(${LIBEIO_LIBRARY} eio_custom  "" EIO_CUSTOM_IS_PRESENT)
+if (NOT EIO_CUSTOM_IS_PRESENT)
+	message(FATAL_ERROR "symbol eio_custom is not found in ${LIBEIO_LIBRARY}")
+endif()
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibEio DEFAULT_MSG LIBEIO_LIBRARY LIBEIO_INCLUDE_DIR)
 mark_as_advanced(LIBEIO_INCLUDE_DIR LIBEIO_LIBRARY)
